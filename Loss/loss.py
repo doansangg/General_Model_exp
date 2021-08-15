02e1c4fafe45c552b27f5640eeae1c447617d4d8
doansangg <doansangk54@gmail.com>
2021-08-15 22:18:01 +0700

    update

--- Loss/loss.py
+++ Loss/loss.py
@@ -186,6 +186,7 @@ def log_cosh_dice_bce_loss(pred, mask):
     bce = F.binary_cross_entropy_with_logits(pred, mask, reduction='none')
     
     pred = torch.sigmoid(pred)
+    
     inter = pred * mask
     union = pred + mask
     iou = 1 - (2. * inter + 1) / (union + 1)
